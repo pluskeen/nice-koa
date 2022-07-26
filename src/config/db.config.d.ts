65c05964094ccca7e455940330a9bab8b639f6fd
dengpei <dengpei@shubidatech.com>
2022-07-26 14:29:09 +0800

    chore: package update

---
+++ src/config/db.config.d.ts
@@ -0,0 +1,14 @@
+declare const SESSION_SECRET_KEY = "JX92*&dx_!&";
+declare let REDIS_CONF: {
+  host: string;
+  port: number;
+  password: string;
+};
+declare let MYSQL_CONF: {
+  host: string;
+  port: string;
+  user: string;
+  password: string;
+  database: string;
+};
+export { REDIS_CONF, MYSQL_CONF, SESSION_SECRET_KEY };
